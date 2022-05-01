f8b66886a7b322a9c22128bb6404e4d17f0e9c2e
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-05-01 21:18:13 +0200

    Add MongoDB configuration class

---
+++ src/main/java/se/iths/mongo/MongoDbConfiguration.java
@@ -0,0 +1,12 @@
+package se.iths.mongo;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.naming.Named;
+
+@ConfigurationProperties("db")
+public interface MongoDbConfiguration extends Named {
+
+    @NonNull
+    String getCollection();
+}
